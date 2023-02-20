b65144db081ee508a384445ffc1ccb49e45e0e0a
jorgesoares2997 <jorgesoares2997@gmail.com>
2023-02-20 11:10:30 -0300

    portfolio

---
+++ script.js
@@ -0,0 +1,80 @@
+
+function viewa(){
+    let res = document.getElementById('res')
+    let view = document.getElementById('a')
+    let a = document.getElementById('a')
+    let i = String(view.value)
+    if(i.value == a ){
+   
+    window.alert('[ERRO]')}
+    else{
+        
+        res.innerHTML = `<h2>Formado técnico em Contrabaixo eletrico,
+        <br> bacharelando em Contrabaixo acústico,
+        <br> apaixonado pela musica e por vários instrumentos,
+        <br> atuo profissionalmente em 
+        <br> <strong>concertos</strong> e Festivais de <strong>Jazz</strong>. </h2>`
+    }
+}
+function viewb(){
+    let res = document.getElementById('res')
+    let view = document.getElementById('a')
+    let a = document.getElementById('a')
+    let i = String(view.value)
+    if(i.value == a ){
+   
+    window.alert('[ERRO]')}
+    else{
+        res.innerHTML = `<h2>Por me divertir com qualquer instrumento,
+        <br> aprendi vários deles e
+        <br> hoje ensino música na <abbr title="Escola de Música Elias Mendes Barbosa"><strong>EMEMB</strong>,
+        <br> projeto social que fundei e
+        <br> administro com a ajuda da minha esposa.</abbr></h2>`
+    }
+}
+function viewc(){
+    let res = document.getElementById('res')
+    let view = document.getElementById('a')
+    let a = document.getElementById('a')
+    let i = String(view.value)
+    if(i.value == a ){
+   
+    window.alert('[ERRO]')}
+    else{
+        res.innerHTML = `<h2>Afrente de projetos de edição de audio,
+        <br> <strong>podcasts</strong>, projetos sociais e etc,
+        <br> liderei várias equipes de diversos tamanhos,
+        <br> atuando como <strong>gestor</strong>, <strong>liderado</strong>, <strong>lider</strong> e <strong>co-lider</strong>,
+        <br> desenvolvi um grande senso de posicionamento. </h2>`
+    }
+}
+function viewd(){
+    let res = document.getElementById('res')
+    let view = document.getElementById('a')
+    let a = document.getElementById('a')
+    let i = String(view.value)
+    if(i.value == a ){
+   
+    window.alert('[ERRO]')}
+    else{
+        
+        res.innerHTML = `<h2>Minha veia artistica me trouxe pra mais esse desafio,
+        <br> a programação tem me enchido os olhos,
+        <br> e a agenda, tenho afiado aos poucos
+        <br> meus conhecimentos em <strong><abbr title="Hypertext Markup Language">HTML5</abbr></strong>, <strong><abbr title="Cascading Style Sheets">CSS3</abbr></strong> e <strong><abbr title="JavaScript">JS</abbr></strong>,
+        <br> além da minha porta de entrada pra essa realidade
+        <br> que foi a linguagem <strong>Dart</strong> e o framework <strong>Flutter</strong>.</h2>`
+    }
+}
+function entera(){
+    let res = document.getElementById('res')
+    let enter = document.getElementById('a')
+    let div = document.getElementById('txt')
+    if(enter.value == 0){
+        window.alert('deu errado')
+    }
+    else{
+        // window.alert('deu certo')
+        div.innerHTML = `<p>aqui</p>`
+    }
+}
